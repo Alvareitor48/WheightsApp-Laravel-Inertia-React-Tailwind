fbb72d6a61cd0d0ada1d74d46c93f7257fb7d4ce
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-14 13:42:52 +0100

    Created the Home Component and modify the Welcome route to redirect to Home

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Head} from "@inertiajs/react";
+
+export default function Home() {
+    return (
+        <>
+            <Head title="Home" />
+            <h1 className="text-4xl font-bold text-gray-800">
+                Home
+            </h1>
+        </>
+    )
+    ;
+}
