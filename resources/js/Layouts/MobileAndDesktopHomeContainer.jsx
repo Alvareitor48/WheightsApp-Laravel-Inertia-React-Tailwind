bc8b233d6b4ba5324cebea82b74dd427c74b653d
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-14 21:40:26 +0100

    Do MobileAndDesktopHomeContainer a component and reutilice it

--- resources/js/Layouts/MobileAndDesktopHomeContainer.jsx
+++ resources/js/Layouts/MobileAndDesktopHomeContainer.jsx
@@ -1,10 +1,10 @@
 import desktopAndMobile from '@/assets/images/desktopAndMobile.png'
 import { motion } from 'motion/react'
 
-export const MobileAndDesktopHomeContainer = () => {
+export const MobileAndDesktopHomeContainer = ({Imageorientation,title,li}) => {
     return (
         <div className='mt-40 relative grid grid-cols-2 grid-rows-2 custom-flex-wrap-first-title:grid-cols-1 custom-flex-wrap-first-title:grid-rows-3'>
-            <motion.h2 className={`mb-16 text-responsive-h2 inline-block col-start-2 col-end-3 row-start-1 row-end-2 custom-flex-wrap-first-title:row-start-1 custom-flex-wrap-first-title:row-end-2 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-first-title:pt-96 custom-flex-wrap-little-devices:pt-16`}
+            <motion.h2 className={`mb-16 text-responsive-h2 inline-block ${Imageorientation == 'left' ? 'col-start-2 col-end-3' : 'col-start-1 col-end-2'} row-start-1 row-end-2 custom-flex-wrap-first-title:row-start-1 custom-flex-wrap-first-title:row-end-2 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-first-title:pt-96 custom-flex-wrap-little-devices:pt-16`}
                        initial={{ opacity: 0, x: "60%"}}
                        whileInView={{ opacity: 1, x: 0}}
                        transition={{ duration: 1 }}
@@ -12,8 +12,8 @@ export const MobileAndDesktopHomeContainer = () => {
                            margin:"-115px",
                            once:true
                        }}
-            >CONTROLA TUS RUTINAS</motion.h2>
-            <motion.div className={`flex justify-center items-center col-start-1 col-end-2 row-start-1 row-end-3 custom-flex-wrap-first-title:row-start-2 custom-flex-wrap-first-title:row-end-3 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-little-devices:-mt-14`}
+            >{title}</motion.h2>
+            <motion.div className={`flex justify-center items-center${Imageorientation == 'left' ? 'col-start-1 col-end-2' : 'col-start-2 col-end-3'} row-start-1 row-end-3 custom-flex-wrap-first-title:row-start-2 custom-flex-wrap-first-title:row-end-3 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-little-devices:-mt-14`}
                         initial={{ opacity: 0, scale:0 }}
                         whileInView={{ opacity: 1, scale:1 }}
                         transition={{ duration: 1 }}
@@ -25,7 +25,7 @@ export const MobileAndDesktopHomeContainer = () => {
                     <img className='h-responsive-height-second-image z-2 opacity-70' src={desktopAndMobile} alt='Desktop and mobile view' draggable="false"></img>
                 </div>
             </motion.div>
-            <motion.div className={`-m-24 flex justify-center col-start-2 col-end-3 row-start-2 row-end-3 custom-flex-wrap-first-title:row-start-3 custom-flex-wrap-first-title:row-end-4 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-first-title:mt-6`}
+            <motion.div className={`-m-24 flex justify-center ${Imageorientation == 'left' ? 'col-start-2 col-end-3' : 'col-start-1 col-end-2'} row-start-2 row-end-3 custom-flex-wrap-first-title:row-start-3 custom-flex-wrap-first-title:row-end-4 custom-flex-wrap-first-title:col-start-1 custom-flex-wrap-first-title:col-end-2 custom-flex-wrap-first-title:mt-6`}
                         initial={{ opacity: 0, x: "60%" }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ duration: 1 }}
@@ -33,12 +33,8 @@ export const MobileAndDesktopHomeContainer = () => {
                             once:true
                         }}
             >
-                <ul className={`text-responsive-ul text-start custom-flex-wrap-first-title:text-center`}>
-                    <li>Registro de <span className="text-lilaPrincipal">series y repes</span></li>
-                    <li>Control preciso del RIR</li>
-                    <li><span className="text-lilaPrincipal">Acceso rapido</span> al historial</li>
-                    <li>Control de las cargas</li>
-                    <li>Monitoreo de <span className="text-lilaPrincipal">descansos</span></li>
+                <ul className={`text-responsive-ul ${Imageorientation == 'left' ? 'text-start' : 'text-end'} custom-flex-wrap-first-title:text-center`}>
+                    {li}
                 </ul>
             </motion.div>
         </div>
--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -11,6 +11,27 @@ export default function Home() {
         <MainLayout>
             <Head title="Home" />
             <FirstHomeMainContainer></FirstHomeMainContainer>
-            <MobileAndDesktopHomeContainer></MobileAndDesktopHomeContainer>
+            <MobileAndDesktopHomeContainer
+                Imageorientation={'left'}
+                title={'CONTROLA TUS RUTINAS'}
+                li={<>
+                    <li>Registro de <span className="text-lilaPrincipal">series y repes</span></li>
+                    <li>Control preciso del RIR</li>
+                    <li><span className="text-lilaPrincipal">Acceso rapido</span> al historial</li>
+                    <li>Control de las cargas</li>
+                    <li>Monitoreo de <span className="text-lilaPrincipal">descansos</span></li>
+                </>}>
+            </MobileAndDesktopHomeContainer>
+            <MobileAndDesktopHomeContainer
+                Imageorientation={'right'}
+                title={'PRIORIZA TUS OBJETIVOS'}
+                li={<>
+                    <li className="text-lilaPrincipal">Rutinas personalizadas</li>
+                    <li>Hechas por profesionales</li>
+                    <li>Conoce tus <span className="text-lilaPrincipal">calorias</span> necesarias</li>
+                    <li>Busca tu objetivo</li>
+                    <li>Calcula tu <span className="text-lilaPrincipal">repeticion maxima</span></li>
+                </>}>
+            </MobileAndDesktopHomeContainer>
         </MainLayout>
     );}
