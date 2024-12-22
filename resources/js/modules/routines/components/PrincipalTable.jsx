e7aa88b87c9f3ab0fd82a27bc63664bdc379b35c
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-22 23:05:23 +0100

    Error during create a new serie fixed

--- resources/js/modules/routines/components/PrincipalTable.jsx
+++ resources/js/modules/routines/components/PrincipalTable.jsx
@@ -15,11 +15,12 @@ export function PrincipalTable({index,series}) {
         const updatedExercises = [...data.exercises];
         const exerciseId = updatedExercises[index].data.id;
         const serie = {
-            ...updatedExercises[index].data.series[index][0],
             id: `temp-${exerciseId}-${updatedExercises[index].data.series[index].length}`,
             RIR: 0,
             repetitions: 0,
             weight: 0,
+            duration: "00:00:00",
+            failure: false,
         };
         updatedExercises[index].data.series[index].push(serie)
         setData("exercises", updatedExercises);
