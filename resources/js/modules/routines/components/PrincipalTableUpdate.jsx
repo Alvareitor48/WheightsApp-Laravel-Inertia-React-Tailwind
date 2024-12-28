f00b7169e3adac126ce1692508ce6bc1cf60f763
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-28 16:11:57 +0100

    PrincipalTableBase component created to reuse table logic in PrincipalTableShow and PrincipalTableUpdate

--- resources/js/modules/routines/components/PrincipalTableUpdate.jsx
+++ resources/js/modules/routines/components/PrincipalTableUpdate.jsx
@@ -3,81 +3,60 @@ import { GridTableUpdate } from "@/modules/routines/components/GridTableUpdate.j
 import { m } from "motion/react"
 import { useUpdate } from '@/modules/routines/hooks/useUpdate';
 import { useCreateSeries } from '../hooks/useCreateSeries';
+import { PrincipalTableBase } from './PrincipalTableBase';
 export function PrincipalTableUpdate({ index, series }) {
     const { update, data } = useUpdate()
     const { createSeries } = useCreateSeries()
+    const headDivs = [
+        <div className='flex justify-center m-1'>
+            <div
+                className="h-responsive-height-table-image w-responsive-width-table-image rounded-full bg-cover bg-center"
+                style={{ backgroundImage: `url(${shoulder_press})` }}
+            ></div>
+        </div>,
+        <div className='text-center px-2 py-3'>
+            <h2 className='text-responsive-table font-semibold inline-block'>{data.exercises[index].data.exercise[index].name}</h2>
+        </div>,
+        <div className='text-center'>
+            <textarea
+                rows={1}
+                className='text-responsive-note-table leading-normal resize-none w-responsive-mini-input text-gray-400 inline-block bg-transparent border-0'
+                value={data.exercises[index].data.note}
+                onChange={(e) => update(e.target.value, true, 'note', index)}
+                onInput={(e) => {
+                    e.target.style.height = "auto";
+                    e.target.style.height = `${e.target.scrollHeight}px`;
+                }}
+            />
+        </div>,
+        <div className='text-center px-2 py-3'>
+            <p className='text-responsive-table font-medium text-gray-400 inline-block'>03:00</p>
+        </div>
+    ]
+
+    const tbody = series.map(function (serie, seriesIndex) {
+        return (
+            <GridTableUpdate key={`${serie.id}.${index}`} seriesIndex={seriesIndex}
+                principalIndex={index}></GridTableUpdate>
+        )
+    });
+
+    const aditionalDivs = (
+        <m.button
+            type="button"
+            className="glass pb-1 mt-5 w-full h-responsive-normal-button-height text-responsive-h4"
+            whileHover={{ scale: 1.1 }}
+            onClick={(e) => {
+                e.preventDefault();
+                createSeries(index);
+            }}
+        >
+            + Añadir Serie
+        </m.button>
+    )
     return (
         <>
-            <div className="m-auto w-[90%] mt-6 mb-6">
-
-                <div
-                    className="border border-gray-300 border-b-0 rounded-t-xl glasstop grid grid-cols-4 items-center justify-between mt-8 mb-1">
-
-                    <div className="flex justify-center m-1">
-                        <div
-                            className="h-responsive-height-table-image w-responsive-width-table-image rounded-full bg-cover bg-center"
-                            style={{ backgroundImage: `url(${shoulder_press})` }}
-                        ></div>
-                    </div>
-
-                    {/* Título del Ejercicio */}
-                    <div className="text-center px-2 py-3">
-                        <h2 className="text-responsive-table font-semibold inline-block">{data.exercises[index].data.exercise[index].name}</h2>
-                    </div>
-
-                    {/* Notas */}
-                    <div className="text-center">
-                        <textarea
-                            rows={1}
-                            className="text-responsive-note-table leading-normal resize-none w-responsive-mini-input text-gray-400 inline-block bg-transparent border-0"
-                            value={data.exercises[index].data.note}
-                            onChange={(e) => update(e.target.value, true, 'note',index)}
-                            onInput={(e) => {
-                                e.target.style.height = "auto"; // Resetea el alto
-                                e.target.style.height = `${e.target.scrollHeight}px`; // Ajusta al contenido
-                            }}
-                        />
-                    </div>
-
-                    {/* Temporizador (No funcional) */}
-                    <div className="text-center px-2 py-3">
-                        <p className="text-responsive-table font-medium text-gray-400 inline-block">03:00</p>
-                    </div>
-                </div>
-
-                <table
-                    className="text-responsive-table border-separate border-spacing-0 glassbottom border border-gray-300 border-t-0 rounded-b-xl w-full">
-                    <thead>
-                        <tr className="text-white uppercase leading-normal">
-                            <th className="w-1/4 py-3 px-2 text-center">Serie</th>
-                            <th className="w-1/4 py-3 px-2 text-center">Reps</th>
-                            <th className="w-1/4 py-3 px-2 text-center">Peso</th>
-                            <th className="w-1/4 py-3 px-2 text-center">RIR</th>
-                        </tr>
-                    </thead>
-                    <tbody className="text-gray-700">
-                        {
-                            series.map(function (serie, seriesIndex) {
-                                return (
-                                    <GridTableUpdate key={`${serie.id}.${index}`} seriesIndex={seriesIndex}
-                                        principalIndex={index}></GridTableUpdate>
-                                )
-                            })
-                        }
-                    </tbody>
-                </table>
-                <m.button
-                    type="button"
-                    className="glass pb-1 mt-5 w-full h-responsive-normal-button-height text-responsive-h4"
-                    whileHover={{ scale: 1.1 }}
-                    onClick={(e) => {
-                        e.preventDefault();
-                        createSeries(index);
-                    }}
-                >
-                    + Añadir Serie
-                </m.button>
-            </div>
+            <PrincipalTableBase headDivs={headDivs} tbody={tbody} aditionalDivs={aditionalDivs}></PrincipalTableBase>
         </>
     );
 }
