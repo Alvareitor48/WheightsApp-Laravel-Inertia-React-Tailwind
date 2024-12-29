4a2f38207615bba2fdbb4a24bc57b6f26840faa5
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-29 10:55:11 +0100

    Type button added to the component to dont do a submit

--- resources/js/modules/routines/components/RouteButton.jsx
+++ resources/js/modules/routines/components/RouteButton.jsx
@@ -1,6 +1,7 @@
 import { m } from "motion/react";
 export const RouteButton = ({onClick,title}) => {
     return(<m.button
+                type="button"
                 className="bg-lilaPrincipal pb-1 mt-10 w-responsive-normal-button-width h-responsive-normal-button-height text-responsive-h4 rounded-xl"
                 whileHover={{backgroundColor: "#8F3985", scale: 1.1}}
                 onClick={onClick}
