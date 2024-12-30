488c144db169619073cf431f3f5edac0b46aab6d
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2024-12-30 19:40:01 +0100

    Validated messages added successfully

--- app/Http/Requests/UpdateRoutinesRequest.php
+++ app/Http/Requests/UpdateRoutinesRequest.php
@@ -44,4 +44,42 @@ class UpdateRoutinesRequest extends FormRequest
             'exercises.*.data.series.*.*.weight' => 'required|numeric',
         ];
     }
+
+    public function messages(): array
+    {
+        return [
+            'routine.id.required' => 'El ID de la rutina es obligatorio',
+            'routine.id.integer' => 'El ID de la rutina debe ser un número entero',
+            'routine.id.exists' => 'El ID de la rutina seleccionado no es válido',
+            'routine.name.required' => 'El nombre de la rutina es obligatorio',
+            'routine.name.string' => 'El nombre de la rutina debe ser una cadena de texto',
+            'routine.name.max' => 'El nombre de la rutina no puede tener más de 255 caracteres',
+            'routine.description.string' => 'La descripción de la rutina debe ser una cadena de texto',
+            'routine.description.max' => 'La descripción de la rutina no puede tener más de 1000 caracteres',
+            'routine.day.required' => 'El día de la rutina es obligatorio',
+            'routine.day.string' => 'El día de la rutina debe ser una cadena de texto',
+            'routine.day.in' => 'El día de la rutina debe ser uno de los siguientes: Lunes, Martes, Miércoles, Jueves, Viernes',
+            'exercises.*.data.id.required' => 'El ID de la rutina de ejercicios es obligatorio',
+            'exercises.*.data.id.integer' => 'El ID de la rutina de ejercicios debe ser un número entero',
+            'exercises.*.data.id.exists' => 'El ID de la rutina de ejercicios seleccionado no es válido',
+            'exercises.*.data.note.required' => 'La nota del ejercicio es obligatoria',
+            'exercises.*.data.note.string' => 'La nota del ejercicio debe ser una cadena de texto',
+            'exercises.*.data.note.max' => 'La nota del ejercicio no puede tener más de 255 caracteres',
+            'exercises.*.data.exercise.*.id.required' => 'El ID del ejercicio es obligatorio',
+            'exercises.*.data.exercise.*.id.integer' => 'El ID del ejercicio debe ser un número entero',
+            'exercises.*.data.exercise.*.id.exists' => 'El ID del ejercicio seleccionado no es válido',
+            'exercises.*.data.exercise.*.name.required' => 'El nombre del ejercicio es obligatorio',
+            'exercises.*.data.exercise.*.name.string' => 'El nombre del ejercicio debe ser una cadena de texto',
+            'exercises.*.data.exercise.*.name.max' => 'El nombre del ejercicio no puede tener más de 255 caracteres',
+            'exercises.*.data.series.*.*.id.required' => 'El ID de la serie es obligatorio',
+            'exercises.*.data.series.*.*.repetitions.required' => 'El número de repeticiones es obligatorio',
+            'exercises.*.data.series.*.*.repetitions.integer' => 'El número de repeticiones debe ser un número entero',
+            'exercises.*.data.series.*.*.RIR.required' => 'El RIR es obligatorio',
+            'exercises.*.data.series.*.*.RIR.in' => 'El RIR debe ser uno de los siguientes: 0, 1, 2, 3, 4, 5',
+            'exercises.*.data.series.*.*.failure.required' => 'El campo de fallo es obligatorio',
+            'exercises.*.data.series.*.*.failure.boolean' => 'El campo de fallo debe ser verdadero o falso',
+            'exercises.*.data.series.*.*.weight.required' => 'El peso es obligatorio',
+            'exercises.*.data.series.*.*.weight.numeric' => 'El peso debe ser un número',
+        ];
+    }
 }
