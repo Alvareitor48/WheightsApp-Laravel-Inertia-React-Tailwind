f227ed3cb64284b5de6928ed47101ce1a6997e7c
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-01-08 16:26:51 +0100

    Fix dashboard route error

--- app/Http/Controllers/Auth/EmailVerificationPromptController.php
+++ app/Http/Controllers/Auth/EmailVerificationPromptController.php
@@ -16,7 +16,7 @@ class EmailVerificationPromptController extends Controller
     public function __invoke(Request $request): RedirectResponse|Response
     {
         return $request->user()->hasVerifiedEmail()
-                    ? redirect()->intended(route('profile/pages/Dashboard', absolute: false))
+                    ? redirect()->intended(route('dashboard', absolute: false))
                     : Inertia::render('auth/pages/VerifyEmail', ['status' => session('status')]);
     }
 }
