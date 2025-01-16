5e3381f5fdf000c38ec7674137ecd63041e8df07
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-01-16 10:34:59 +0100

    SerieChecked Context added to manage completed series

---
+++ resources/js/modules/routines/contexts/SerieCheckedContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState } from "react";
+
+const SerieCheckedContext = createContext();
+
+export const SerieCheckedProvider = ({ children }) => {
+    const [completedSeries, setCompletedSeries] = useState({});
+
+    const toggleCompletion = (principalIndex, seriesIndex) => {
+        const seriesKey = `${principalIndex}-${seriesIndex}`;
+        setCompletedSeries((previousState) => {
+            const newState = { ...previousState };
+            newState[seriesKey] = !newState[seriesKey];
+            return newState;
+        });
+    };
+
+    const areExerciseSeriesCompleted = (exerciseIndex, seriesCount) => {
+        const completedCount = Object.keys(completedSeries).filter(
+            (key) => key.startsWith(`${exerciseIndex}-`) && completedSeries[key]
+        ).length;
+        return completedCount === seriesCount;
+    };
+
+    return (
+        <SerieCheckedContext.Provider
+            value={{
+                completedSeries,
+                toggleCompletion,
+                areExerciseSeriesCompleted,
+            }}
+        >
+            {children}
+        </SerieCheckedContext.Provider>
+    );
+};
+
+// Hook para consumir el contexto
+export const useSerieChecked = () => {
+    const context = useContext(SerieCheckedContext);
+    if (!context) {
+        throw new Error(
+            "useSerieChecked debe usarse dentro de SerieCheckedProvider"
+        );
+    }
+    return context;
+};
