429289af6cbc0382a1feed5986ad074100273c2b
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-01-17 10:09:59 +0100

    Refactor RoutineController to save a session correctly

--- app/Http/Controllers/RoutineController.php
+++ app/Http/Controllers/RoutineController.php
@@ -6,9 +6,12 @@ use App\Http\Resources\ExerciseResource;
 use App\Http\Resources\ExerciseRoutineResource;
 use App\Http\Resources\SerieResource;
 use App\Models\Exercise;
+use App\Models\ExerciseLog;
 use App\Models\ExerciseRoutine;
 use App\Models\Routine;
+use App\Models\RoutineSession;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Http\Requests\UpdateRoutinesRequest;
@@ -16,12 +19,70 @@ use App\Http\Requests\StartRoutinesRequest;
 
 class RoutineController extends Controller
 {
-    public function index(){
-        $user = User::first();
-        return Inertia::render('routines/pages/IndexRoutines', [
-            'routines' => $user->routines,
+    private function updateRoutineAndSeries(array $data)
+    {
+        $routine = Routine::query()->where('id', $data['routine']['id'])->firstOrFail();
+        $routine->update([
+            'name' => $data['routine']['name'],
+            'description' => $data['routine']['description'],
+            'day' => $data['routine']['day'],
         ]);
+
+        $updatedExercises = [];
+
+        foreach ($data['exercises'] as $exerciseData) {
+            $exerciseRoutine = ExerciseRoutine::findOrFail($exerciseData['id']);
+            $exerciseRoutine->update([
+                'note' => $exerciseData['note'],
+            ]);
+
+            $seriesGroups = $exerciseData['series'] ?? [];
+            $seriesIds = collect($seriesGroups)
+                ->filter(fn($seriesData) => is_numeric($seriesData['id']))
+                ->pluck('id')
+                ->toArray();
+            $exerciseRoutine->series()->whereNotIn('id', $seriesIds)->delete();
+
+            $updatedSeries = [];
+
+            if (!empty($exerciseData['series'])) {
+                foreach ($exerciseData['series'] as $seriesData) {
+                    if (is_numeric($seriesData['id'])) {
+                        $series = $exerciseRoutine->series()->find($seriesData['id']);
+                        $series?->update([
+                            'repetitions' => $seriesData['repetitions'],
+                            'weight' => $seriesData['weight'],
+                            'RIR' => $seriesData['RIR'],
+                            'failure' => $seriesData['failure'],
+                        ]);
+                        $updatedSeries[] = $series;
+                    } elseif (preg_match('/^[0-9a-fA-F-]{36}$/', $seriesData['id'])) {
+                        $series = $exerciseRoutine->series()->create([
+                            'repetitions' => $seriesData['repetitions'],
+                            'weight' => $seriesData['weight'],
+                            'RIR' => $seriesData['RIR'],
+                            'failure' => $seriesData['failure'],
+                        ]);
+                        $updatedSeries[] = $series;
+                    }
+                }
+            }
+
+            $updatedExercises[] = [
+                'id' => $exerciseRoutine->id,
+                'note' => $exerciseRoutine->note,
+                'exercise' => new ExerciseResource(Exercise::find($exerciseRoutine->exercise_id)),
+                'series' => SerieResource::collection($updatedSeries),
+            ];
+        }
+
+        return [
+            'routine' => $routine,
+            'exercises' => $updatedExercises,
+        ];
     }
+
+
     private function getRoutineDetails($id){
         $routine = Routine::with('user')->where('id', $id)->firstOrFail();
         $exerciseRoutines = ExerciseRoutine::with('series')->where('routine_id',$routine->id)->get();
@@ -39,6 +100,12 @@ class RoutineController extends Controller
             'exercises' => $exerciseRoutinesWithDetails,
         ];
     }
+    public function index(){
+        $user = User::first();
+        return Inertia::render('routines/pages/IndexRoutines', [
+            'routines' => $user->routines,
+        ]);
+    }
     public function show($id){
         $routineDetails = $this->getRoutineDetails($id);
         return Inertia::render('routines/pages/AdminRoutines', $routineDetails);
@@ -51,52 +118,12 @@ class RoutineController extends Controller
 
     public function update(UpdateRoutinesRequest $request){
         $data = $request->all();
-        $routine = Routine::query()->where('id', $data['routine']['id'])->firstOrFail();
-        $routine->update([
-            'name' => $data['routine']['name'],
-            'description' => $data['routine']['description'],
-            'day' => $data['routine']['day'],
-        ]);
-
-        foreach ($data['exercises'] as $exerciseData) {
-            $exerciseRoutine = ExerciseRoutine::findOrFail($exerciseData['id']);
-            $exerciseRoutine->update([
-                'note' => $exerciseData['note'],
-            ]);
-            $seriesGroups = $exerciseData['series'] ?? [];
-            $seriesIds = collect($seriesGroups)
-                ->filter(fn($seriesData) => is_numeric($seriesData['id']))
-                ->pluck('id')
-                ->toArray();
-            $exerciseRoutine->series()->whereNotIn('id', $seriesIds)->delete();
-
-            
-            if (!empty($exerciseData['series'])) {
-                foreach ($exerciseData['series'] as $seriesData) {
-                    if (is_numeric($seriesData['id'])) {
-                        $series = $exerciseRoutine->series()->find($seriesData['id']);
-                        $series?->update([
-                            'repetitions' => $seriesData['repetitions'],
-                            'weight' => $seriesData['weight'],
-                            'RIR' => $seriesData['RIR'],
-                            'failure' => $seriesData['failure'],
-                        ]);
-                    } elseif (preg_match('/^[0-9a-fA-F-]{36}$/', $seriesData['id'])) {
-                        $exerciseRoutine->series()->create([
-                            'repetitions' => $seriesData['repetitions'],
-                            'weight' => $seriesData['weight'],
-                            'RIR' => $seriesData['RIR'],
-                            'failure' => $seriesData['failure'],
-                        ]);
-                    }
-                }
-            }
-        }
+        $routineAndExercises = $this->updateRoutineAndSeries($data);
         if ($request->expectsJson()) {
             return response()->json(['status' => 'Rutina actualizada correctamente.']);
         }
-    
-        return redirect()->route('AdminRoutines', ['id' => $routine->id])
+
+        return redirect()->route('AdminRoutines', ['id' => $routineAndExercises['routine']->id])
             ->with('success', 'Rutina actualizada correctamente.');
     }
 
@@ -107,6 +134,25 @@ class RoutineController extends Controller
     }
 
     public function session(StartRoutinesRequest $request){
-        dd($request->all());
+        $data = $request->all();
+        $updateData = $this->updateRoutineAndSeries($data);
+        $routineSession = RoutineSession::create([
+            'routine_id' => $updateData['routine']['id'],
+            'user_id'=> auth()->user()->id,
+            'duration' => Carbon::createFromTimestampUTC($data['routine']['durationInSeconds'])->toTimeString()
+        ]);
+
+        foreach ($updateData['exercises'] as $exercise) {
+            foreach ($exercise['series'] as $series) {
+                ExerciseLog::create([
+                    'routine_session_id' => $routineSession->id,
+                    'exercise_id' => $exercise['exercise']['id'],
+                    'series_id' => $series['id'],
+                ]);
+            }
+        }
+
+        return redirect()->route('AdminRoutines', ['id' => $updateData['routine']['id']])
+            ->with('success', 'Rutina actualizada correctamente.');
     }
 }
