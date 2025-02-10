3216a45388005a84254249fe8a0f1e90ac04f00d
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-10 21:48:57 +0100

    Exercise Filter by muscle or equipment added to components

--- resources/js/modules/exercises/pages/AddExercises.jsx
+++ resources/js/modules/exercises/pages/AddExercises.jsx
@@ -2,16 +2,115 @@ import MainLayout from "@/shared/layouts/MainLayout";
 import { m } from "motion/react";
 import Pagination from "../components/Pagination";
 import ExerciseCard from "../components/ExerciseCard";
-import { router } from "@inertiajs/react";
+import { router, useForm } from "@inertiajs/react";
 import { CardRouteButton } from "../components/CardRouteButton";
 
-export default function IndexExercises({ exercises, routineId, redirect_to }) {
+export default function AddExercises({
+    exercises,
+    routineId,
+    redirect_to,
+    equipments,
+    muscles,
+}) {
+    const { data, setData, get } = useForm({
+        equipment: "",
+        muscle: "",
+    });
+
+    const handleFilterChange = (e) => {
+        setData(e.target.name, e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        get(route("routines.add.exercises", { routineId, redirect_to }), {
+            params: data,
+            preserveState: true,
+        });
+    };
+
+    const handleClearFilters = () => {
+        setData({ equipment: "", muscle: "" });
+        get(route("routines.add.exercises", { routineId, redirect_to }), {
+            preserveState: true,
+        });
+    };
     return (
         <MainLayout>
             <m.div className="min-h-screen bg-transparent p-8 relative">
                 <h1 className="text-responsive-h2 font-bold text-white mb-8 text-center">
                     Ejercicios
                 </h1>
+                <form
+                    onSubmit={handleSubmit}
+                    className="bg-transparent p-6 rounded shadow-lg mb-8"
+                >
+                    <div className="flex flex-col">
+                        <label className="text-sm font-semibold text-white">
+                            Equipamiento
+                        </label>
+                        <select
+                            name="equipment"
+                            value={data.equipment}
+                            onChange={handleFilterChange}
+                            className="border bg-transparent border-gray-300 rounded p-2 mt-1"
+                        >
+                            <option value="">Todos</option>
+                            {equipments.map((equip, index) =>
+                                equip ? (
+                                    <option key={index} value={equip}>
+                                        {equip}
+                                    </option>
+                                ) : null
+                            )}
+                            <option value={"Sin equipamiento"}>
+                                Sin equipamiento
+                            </option>
+                        </select>
+                    </div>
+
+                    <div className="flex flex-col mt-4">
+                        <label className="text-sm font-semibold text-white">
+                            Músculos
+                        </label>
+                        <select
+                            name="muscle"
+                            value={data.muscle}
+                            onChange={handleFilterChange}
+                            className="border border-gray-300 bg-transparent rounded p-2 mt-1"
+                        >
+                            <option value="">Todos</option>
+                            {muscles.map((muscle, index) => (
+                                <option key={index} value={muscle}>
+                                    {muscle === "Deltoides Frontal"
+                                        ? "Deltoides Frontal y Lateral"
+                                        : muscle}
+                                </option>
+                            ))}
+                        </select>
+                    </div>
+                    <div className="flex space-x-4 mt-4">
+                        <m.button
+                            type="submit"
+                            className="px-4 py-2 bg-lilaPrincipal text-white rounded-md hover:bg-lilaSecundario"
+                            whileHover={{
+                                scale: 1.1,
+                            }}
+                        >
+                            Filtrar
+                        </m.button>
+                        <m.button
+                            type="button"
+                            onClick={handleClearFilters}
+                            className="px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400"
+                            whileHover={{
+                                scale: 1.1,
+                            }}
+                        >
+                            Limpiar
+                        </m.button>
+                    </div>
+                </form>
                 <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                     {exercises.data.map((exercise, index) => (
                         <ExerciseCard
