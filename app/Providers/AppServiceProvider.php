9f4117770c8046df6e1119dbd385487a10cbd63a
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-11 22:14:45 +0100

    Sanctum Middleware and Throttle added to api middlewareGroup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,13 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
+use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
 
@@ -25,6 +27,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(Router $router): void
     {
+        app('router')->middlewareGroup('api', [
+            EnsureFrontendRequestsAreStateful::class, // Middleware de Sanctum
+            ThrottleRequests::class . ':api',
+        ]);
         $router->aliasMiddleware('role', RoleMiddleware::class);
         $router->aliasMiddleware('permission', PermissionMiddleware::class);
         Vite::prefetch(concurrency: 3);
