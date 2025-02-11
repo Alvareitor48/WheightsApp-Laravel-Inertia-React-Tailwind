6864dd50e269f1c2da686c190530492fc332d0f2
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-11 22:34:24 +0100

    AuthApiController created with register method model User updated to use HasApiTokens

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ExerciseApiController;
+use App\Http\Controllers\Api\AuthApiController;
 
+Route::post('/register', [AuthApiController::class, 'register'])->name('api.register');
 Route::get('/exercises', [ExerciseApiController::class, 'index'])->name('api.exercises.index');
