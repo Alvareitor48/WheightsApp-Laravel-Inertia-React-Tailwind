b726f8ec0ff21a5e19523acc578996f6e7327bf0
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-12 00:59:21 +0100

    Roles set in routes middlewares

--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ExerciseApiController;
 use App\Http\Controllers\Api\AuthApiController;
 
-Route::middleware(['auth:sanctum','role:user'])->group(function () {
+Route::middleware(['auth:sanctum','role:user|admin|premium'])->group(function () {
     Route::get('/exercises/{id}', [ExerciseApiController::class, 'show'])->name('api.exercises.show');
     Route::post('/logout', [AuthApiController::class, 'logout'])->name('api.logout');
+});
+
+Route::middleware(['auth:sanctum','role:admin|premium'])->group(function () {
     Route::post('/exercises', [ExerciseApiController::class, 'store'])->name('api.exercises.store');
 });
 
