885fecc9ef6184cc10aafff21a300eb9865c6841
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-14 09:08:53 +0100

    AuthServiceProvider added and registered it on providers

---
+++ app/Providers/AuthServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Exercise;
+use App\Models\Routine;
+use App\Policies\ExercisePolicy;
+use App\Policies\RoutinePolicy;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\ServiceProvider;
+
+class AuthServiceProvider extends ServiceProvider
+{
+    protected $policies = [
+        Exercise::class => ExercisePolicy::class,
+        Routine::class => RoutinePolicy::class,
+    ];
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        foreach ($this->policies as $model => $policy) {
+            Gate::policy($model, $policy);
+        }
+    }
+}
