778da530f9aaec34b0205c97e97ce34be6996de1
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-15 17:12:34 +0100

    DashboardController refactored

--- app/Http/Controllers/Web/DashboardController.php
+++ app/Http/Controllers/Web/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Helpers\DateHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\BodyHighLightDataResource;
 use App\Http\Resources\ExerciseLogByExercisesResource;
@@ -18,29 +19,9 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $today = Carbon::now()->endOfDay();
-        $previousMonth = Carbon::now()->subMonth()->startOfDay();
-        $previousWeek = Carbon::now()->subWeek()->startOfDay();
-        $sessionsQuery = RoutineSession::query()
-                ->where('user_id',auth()->id());
-
-        if(!auth()->user()->hasRole('admin') || !auth()->user()->hasRole('premium')){
-            $sessionsQuery->whereDate('completed_at', '>=', $previousMonth);
-        }
-
-        $sessions = $sessionsQuery->where('user_id',auth()->id())
-            ->whereDate('completed_at', '<=', $today)
-            ->with('routine')
-            ->get();
-
-        $exerciseLogs = ExerciseLog::query()
-            ->whereHas('routine_session', function ($query) use ($previousWeek, $today) {
-                $query->where('user_id', auth()->id())
-                    ->whereBetween('completed_at', [$previousWeek, $today]);
-            })->with('exercise:id,name')
-            ->get()
-            ->groupBy('exercise.name')
-            ->values();
+        [$today, $previousMonth, $previousWeek] = $this->getIndexDates();
+        $sessions = $this->getSessions($previousMonth, $today);
+        $exerciseLogs = $this->getExerciseLogsByDateRange($previousWeek, $today);
 
         return Inertia::render('profile/pages/Dashboard',[
             'sessions'=>SessionsForCalendarResource::collection($sessions)->toArray(request()),
@@ -52,24 +33,84 @@ class DashboardController extends Controller
 
     public function getExercisesByMuscle(Request $request, $muscleName)
     {
-        $today = Carbon::now()->endOfDay();
-        $period = $request->input('period', 'month');
+        [$today, $startDate] = DateHelper::getDays($request->input('period', 'month'));
+        $muscle = Muscle::query()->where('name', $muscleName)->firstOrFail();
+        $exerciseLogs = $this->logsFilter($muscle, $today, $startDate);
+        $finalLogs = $this->groupByCompletedDate($exerciseLogs);
 
-        switch ($period) {
-            case '3months':
-                $startDate = Carbon::now()->subMonths(3)->startOfDay();
-                break;
-            case 'year':
-                $startDate = Carbon::now()->subYear()->startOfDay();
-                break;
-            default:
-                $startDate = Carbon::now()->subMonth()->startOfDay();
-                break;
+        return redirect()->route('dashboard')
+            ->with('exercisesForMuscle', $finalLogs);
+    }
+
+    public function getMaxWeightsByMuscle(Request $request, $muscleName)
+    {
+        [$today, $startDate] = DateHelper::getDays($request->input('period', 'month'));
+        $muscle = Muscle::query()->where('name', $muscleName)->firstOrFail();
+        $logsMaxWeights = $this->logsFilter($muscle, $today, $startDate);
+        $processedLogs = $this->getMaxAndMinWeights($logsMaxWeights);
+
+        return redirect()->route('dashboard')
+            ->with('logsMaxWeights', $processedLogs);
+    }
+
+    private function getIndexDates()
+    {
+        return [
+            Carbon::now()->endOfDay(),
+            Carbon::now()->subMonth()->startOfDay(),
+            Carbon::now()->subWeek()->startOfDay()
+        ];
+    }
+
+    private function getSessions($startDate, $endDate)
+    {
+        $sessionsQuery = RoutineSession::query()
+            ->where('user_id', auth()->id());
+
+        if (!auth()->user()->hasRole('admin') || !auth()->user()->hasRole('premium')) {
+            $sessionsQuery->whereDate('completed_at', '>=', $startDate);
         }
 
-        $muscle = Muscle::query()->where('name', $muscleName)->firstOrFail();
+        return $sessionsQuery->whereDate('completed_at', '<=', $endDate)
+            ->with('routine')
+            ->get();
+    }
 
-        $exerciseLogs = ExerciseLog::query()
+    private function getExerciseLogsByDateRange($startDate, $endDate)
+    {
+        return ExerciseLog::query()
+            ->whereHas('routine_session', function ($query) use ($startDate, $endDate) {
+                $query->where('user_id', auth()->id())
+                    ->whereBetween('completed_at', [$startDate, $endDate]);
+            })
+            ->with('exercise:id,name')
+            ->get()
+            ->groupBy('exercise.name')
+            ->values();
+    }
+
+    private function groupByCompletedDate($exerciseLogs)
+    {
+        return $exerciseLogs->groupBy(fn($log) => $log->routine_session->completed_at)
+            ->map(fn($logs) => (new ExerciseLogByExercisesResource($logs))->toArray(request()))
+            ->values()
+            ->toArray();
+    }
+
+    private function getMaxAndMinWeights($logs)
+    {
+        return $logs->groupBy('exercise.name')
+            ->map(function ($logs) {
+                $maxLog = $logs->sortByDesc(fn($log) => $log->weight * $log->repetitions)->first();
+                $minLog = $logs->sortBy(fn($log) => $log->weight * $log->repetitions)->first();
+                return (new MaxLogResource($maxLog, $minLog))->toArray(request());
+            })
+            ->values();
+    }
+
+    private function logsFilter($muscle, $today, $startDate)
+    {
+        return ExerciseLog::query()
             ->whereHas('routine_session', function ($query) use ($startDate, $today) {
                 $query->where('user_id', auth()->id())
                     ->whereBetween('completed_at', [$startDate, $today]);
@@ -79,60 +120,5 @@ class DashboardController extends Controller
             })
             ->with(['exercise:id,name', 'routine_session:id,completed_at'])
             ->get();
-
-        $groupedByDate = $exerciseLogs->groupBy(function ($log) {
-            return $log->routine_session->completed_at;
-        });
-        $finalLogs = [];
-
-        foreach ($groupedByDate as $logs) {
-            $finalLogs[] = (new ExerciseLogByExercisesResource($logs))->toArray(request());
-        }
-
-        return redirect()->route('dashboard')
-            ->with('exercisesForMuscle', $finalLogs);
-    }
-
-    public function getMaxWeightsByMuscle(Request $request, $muscleName)
-    {
-        $today = Carbon::now()->endOfDay();
-        $period = $request->input('period', 'month');
-
-        switch ($period) {
-            case '3months':
-                $startDate = Carbon::now()->subMonths(3)->startOfDay();
-                break;
-            case 'year':
-                $startDate = Carbon::now()->subYear()->startOfDay();
-                break;
-            default:
-                $startDate = Carbon::now()->subMonth()->startOfDay();
-                break;
-        }
-        $muscle = Muscle::query()->where('name', $muscleName)->firstOrFail();
-        $logsMaxWeights = ExerciseLog::query()
-            ->whereHas('routine_session', function ($query) use ($startDate, $today) {
-                $query->where('user_id', auth()->id())
-                ->whereBetween('completed_at', [$startDate, $today]);
-            })
-            ->whereHas('exercise.muscles', function ($query) use ($muscle) {
-                $query->where('muscles.id', $muscle->id);
-            })
-            ->with(['exercise:id,name', 'routine_session:id,completed_at'])
-            ->get()
-            ->groupBy('exercise.name')
-            ->map(function ($logs) {
-                $maxLog = $logs->sortByDesc(function ($log) {
-                    return $log->weight * $log->repetitions;
-                })->first();
-                $minLog = $logs->sortBy(function ($log) {
-                    return $log->weight * $log->repetitions;
-                })->first();
-                return (new MaxLogResource($maxLog,$minLog))->toArray(request());
-            })
-            ->values();
-
-        return redirect()->route('dashboard')
-            ->with('logsMaxWeights', $logsMaxWeights);
     }
 }
