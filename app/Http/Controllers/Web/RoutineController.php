396bb153fd8f13bd43fe9235840f93ddfd500a71
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-15 17:12:49 +0100

    RoutineController refactored

--- app/Http/Controllers/Web/RoutineController.php
+++ app/Http/Controllers/Web/RoutineController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 
+use App\Helpers\DateHelper;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\ManageExerciseRequest;
 use App\Http\Requests\StartRoutinesRequest;
@@ -25,6 +26,223 @@ use App\Events\RoutineDeleted;
 class RoutineController extends Controller
 {
     use AuthorizesRequests;
+    public function index(){
+        $this->authorize('viewAny', Exercise::class);
+        return Inertia::render('routines/pages/IndexRoutines', [
+            'routines' => auth()->user()->routines,
+        ]);
+    }
+    public function show($id){
+        $routine = Routine::findOrFail($id);
+        $this->authorize('view', $routine);
+        $routineDetails = $this->getRoutineDetails($id);
+        $routineDetails['stadistics'] = $this->getShowStadistics($id);
+
+        return Inertia::render('routines/pages/AdminRoutines', $routineDetails);
+    }
+
+    public function edit($id){
+        $routine = Routine::findOrFail($id);
+        $this->authorize('update', $routine);
+        $routineDetails = $this->getRoutineDetails($id);
+        return Inertia::render('routines/pages/UpdateRoutines', $routineDetails);
+    }
+
+    public function update(UpdateRoutinesRequest $request){
+        $data = $request->all();
+        $routine = Routine::findOrFail($data['routine']['id']);
+        $this->authorize('update', $routine);
+        $routineAndExercises = $this->updateRoutineAndSeries($data);
+        if ($request->expectsJson()) {
+            return response()->json(['status' => 'Rutina actualizada correctamente.']);
+        }
+
+        return redirect()->route('routines.show', ['id' => $routineAndExercises['routine']->id])
+            ->with('success', 'Rutina actualizada correctamente.');
+    }
+
+    public function start($id){
+        $routine = Routine::findOrFail($id);
+        $this->authorize('startRoutine', $routine);
+        $routineDetails = $this->getRoutineDetails($id);
+        $routineDetails['routine']['durationInSeconds'] = 0;
+        return Inertia::render('routines/pages/StartRoutines', $routineDetails);
+    }
+
+    public function session(StartRoutinesRequest $request){
+        $data = $request->all();
+        $routine = Routine::findOrFail($data['routine']['id']);
+        $this->authorize('generatePDF', $routine);
+        $updateDataId = $this->setSessionDataAndGetId($data);
+
+        return redirect()->route('routines.show', ['id' => $updateDataId]);
+    }
+
+    public function addExercise(ManageExerciseRequest $request, $routineId){
+        $routine = Routine::findOrFail($routineId);
+        $this->authorize('addExercise', $routine);
+        $this->addExerciseWithData($request->validated(), $routine);
+        return $this->redirect_to($request->input('redirect_to'), $routineId);
+    }
+
+
+    public function deleteExercise(ManageExerciseRequest $request, $routineId)
+{
+    $routine = Routine::findOrFail($routineId);
+    $this->authorize('addExercise', $routine);
+    $deleted = $this->removeExerciseFromRoutine($routineId, $request->input('exercise_id'));
+
+    if (!$deleted) {
+        return back()->withErrors(['error' => 'El ejercicio no está en esta rutina']);
+    }
+
+    return $this->redirect_to($request->input('redirect_to'), $routineId);
+}
+
+public function store()
+{
+    $this->authorize('create', Routine::class);
+    $routine = Routine::create([
+        'user_id' => auth()->id(),
+        'name' => 'Nueva Rutina',
+        'description' => '',
+    ]);
+
+    return redirect()->route('routines.edit', ['id' => $routine->id]);
+}
+
+    public function updateChart(Request $request, $id)
+    {
+        $routine = Routine::findOrFail($id);
+        $this->authorize('updateChart', $routine);
+
+        session(['stadistics' => $this->chartData($request->input('period'))]);
+
+        return redirect()->route('routines.show', ['id' => $id]);
+    }
+
+    public function generatePDF($id)
+    {
+        $routine = Routine::findOrFail($id);
+        $this->authorize('generatePDF', $routine);
+        $routineDetails = $this->getRoutineDetails($id);
+        $pdf = Pdf::loadView('pdf.routine', ['routineDetails' => $routineDetails]);
+
+        return $pdf->download("rutina_{$routineDetails['routine']['name']}.pdf");
+    }
+
+    public function destroy($id)
+    {
+        $routine = Routine::findOrFail($id);
+        $this->authorize('delete',$routine);
+        event(new RoutineDeleted($routine));
+        $routine->delete();
+
+        return redirect()->route('routines.index');
+    }
+
+    public function restore($id)
+    {
+        $routine = Routine::onlyTrashed()->findOrFail($id);
+        $this->authorize('delete', $routine);
+        $routine->restore();
+
+        return redirect()->route('routines.index');
+    }
+    private function setSessionDataAndGetId($data)
+    {
+        $updateData = $this->updateRoutineAndSeries($data);
+        $routineSession = RoutineSession::create([
+            'routine_id' => $updateData['routine']['id'],
+            'user_id'=> auth()->user()->id,
+            'duration' => Carbon::createFromTimestampUTC($data['durationInSeconds'])->toTimeString()
+        ]);
+
+        foreach ($updateData['exercises'] as $exercise) {
+            foreach ($exercise['series'] as $series) {
+                ExerciseLog::create([
+                    'routine_session_id' => $routineSession->id,
+                    'exercise_id' => $exercise['exercise']['id'],
+                    'repetitions' => $series['repetitions'],
+                    'RIR' => $series['RIR'],
+                    'failure' => $series['failure'],
+                    'weight' => $series['weight'],
+                ]);
+            }
+        }
+        return $updateData['routine']['id'];
+    }
+
+    private function addExerciseWithData($data, Routine $routine)
+    {
+        $exercise = Exercise::query()->where('id', $data['exercise_id'])->firstOrFail();
+        ExerciseRoutine::create([
+            'routine_id' => $routine->id,
+            'exercise_id' => $exercise->id,
+            'note' => null,
+        ]);
+    }
+
+    private function removeExerciseFromRoutine($routineId, $exerciseId)
+    {
+        $exerciseRoutine = ExerciseRoutine::query()
+            ->where('id', $exerciseId)
+            ->where('routine_id', $routineId)
+            ->first();
+
+        if (!$exerciseRoutine) {
+            return false;
+        }
+
+        ExerciseRoutine::query()
+            ->where('routine_id', $routineId)
+            ->where('id', $exerciseId)
+            ->delete();
+
+        return true;
+    }
+
+    private function getShowStadistics($id)
+    {
+        if (session()->has('stadistics')) {
+            $stadistics = session('stadistics');
+            session()->forget('stadistics');
+            return $stadistics;
+        } else {
+            $today = \Illuminate\Support\Carbon::now()->endOfDay();
+            $startDate = \Illuminate\Support\Carbon::now()->subMonth()->startOfDay();
+
+            $sessions = RoutineSession::where('routine_id', $id)
+                ->where('user_id', auth()->user()->id)
+                ->whereBetween('completed_at', [$startDate, $today])
+                ->with('exerciseLogs')
+                ->get();
+
+            return StatisticsResource::collection($sessions)->toArray(request());
+        }
+    }
+
+    private function chartData($period)
+    {
+        [$today, $startDate] = DateHelper::getDays($period);
+        $sessions = RoutineSession::where('user_id', auth()->user()->id)
+            ->whereBetween('completed_at', [$startDate, $today])
+            ->with('exerciseLogs')
+            ->get();
+        return StatisticsResource::collection($sessions)->toArray(request());
+    }
+
+    private function redirect_to($redirect, $routineId){
+        switch ($redirect) {
+            case 'routines.start':
+                return redirect()->route('routines.start', ['id' => $routineId]);
+            case 'routines.edit':
+                return redirect()->route('routines.edit', ['id' => $routineId]);
+            default:
+                return redirect()->route('routines.index');
+        }
+    }
+
     private function updateRoutineAndSeries(array $data)
     {
         $routine = Routine::query()->where('id', $data['routine']['id'])->firstOrFail();
@@ -104,213 +322,6 @@ class RoutineController extends Controller
             'routine' => $routine,
             'exercises' => $exerciseRoutinesWithDetails,
         ];
-    }
-    public function index(){
-        $this->authorize('viewAny', Exercise::class);
-        return Inertia::render('routines/pages/IndexRoutines', [
-            'routines' => auth()->user()->routines,
-        ]);
-    }
-    public function show($id){
-        $routine = Routine::findOrFail($id);
-        $this->authorize('view', $routine);
-        $routineDetails = $this->getRoutineDetails($id);
-        if (session()->has('stadistics')) {
-            $stadistics = session('stadistics');
-            session()->forget('stadistics');
-        } else {
-            $today = \Illuminate\Support\Carbon::now()->endOfDay();
-            $startDate = \Illuminate\Support\Carbon::now()->subMonth()->startOfDay();
-
-            $sessions = RoutineSession::where('routine_id', $id)
-                ->where('user_id', auth()->user()->id)
-                ->whereBetween('completed_at', [$startDate, $today])
-                ->with('exerciseLogs')
-                ->get();
-
-            $stadistics = StatisticsResource::collection($sessions)->toArray(request());
-        }
-
-        $routineDetails['stadistics'] = $stadistics;
-
-        return Inertia::render('routines/pages/AdminRoutines', $routineDetails);
-    }
-
-    public function edit($id){
-        $routine = Routine::findOrFail($id);
-        $this->authorize('update', $routine);
-        $routineDetails = $this->getRoutineDetails($id);
-        return Inertia::render('routines/pages/UpdateRoutines', $routineDetails);
-    }
-
-    public function update(UpdateRoutinesRequest $request){
-        $data = $request->all();
-        $routine = Routine::findOrFail($data['routine']['id']);
-        $this->authorize('update', $routine);
-        $routineAndExercises = $this->updateRoutineAndSeries($data);
-        if ($request->expectsJson()) {
-            return response()->json(['status' => 'Rutina actualizada correctamente.']);
-        }
-
-        return redirect()->route('routines.show', ['id' => $routineAndExercises['routine']->id])
-            ->with('success', 'Rutina actualizada correctamente.');
-    }
-
-    public function start($id){
-        $routine = Routine::findOrFail($id);
-        $this->authorize('startRoutine', $routine);
-        $routineDetails = $this->getRoutineDetails($id);
-        $routineDetails['routine']['durationInSeconds'] = 0;
-        return Inertia::render('routines/pages/StartRoutines', $routineDetails);
-    }
-
-    public function session(StartRoutinesRequest $request){
-        $data = $request->all();
-        $routine = Routine::findOrFail($data['routine']['id']);
-        $this->authorize('generatePDF', $routine);
-        $updateData = $this->updateRoutineAndSeries($data);
-        $routineSession = RoutineSession::create([
-            'routine_id' => $updateData['routine']['id'],
-            'user_id'=> auth()->user()->id,
-            'duration' => Carbon::createFromTimestampUTC($data['durationInSeconds'])->toTimeString()
-        ]);
-
-        foreach ($updateData['exercises'] as $exercise) {
-            foreach ($exercise['series'] as $series) {
-                ExerciseLog::create([
-                    'routine_session_id' => $routineSession->id,
-                    'exercise_id' => $exercise['exercise']['id'],
-                    'repetitions' => $series['repetitions'],
-                    'RIR' => $series['RIR'],
-                    'failure' => $series['failure'],
-                    'weight' => $series['weight'],
-                ]);
-            }
-        }
-
-        return redirect()->route('routines.show', ['id' => $updateData['routine']['id']])
-            ->with('success', 'Rutina actualizada correctamente.');
-    }
-
-    public function addExercise(ManageExerciseRequest $request, $routineId){
-        $routine = Routine::findOrFail($routineId);
-        $this->authorize('addExercise', $routine);
-        $data = $request->validated();
-        $routine = Routine::query()->where('id', $routineId)->firstOrFail();
-        $exercise = Exercise::query()->where('id', $data['exercise_id'])->firstOrFail();
-        $exerciseRoutine = ExerciseRoutine::query()->where('exercise_id', $exercise->id)->where('routine_id', $routine->id);
-        ExerciseRoutine::create([
-            'routine_id' => $routine->id,
-            'exercise_id' => $exercise->id,
-            'note' => null,
-        ]);
-
-        switch ($data['redirect_to']) {
-        case 'routines.start':
-            return redirect()->route('routines.start', ['id' => $routineId]);
-        case 'routines.edit':
-            return redirect()->route('routines.edit', ['id' => $routineId]);
-        default:
-            return redirect()->route('routines.index');
-    }
-
-    }
-
-    public function deleteExercise(ManageExerciseRequest $request, $routineId)
-{
-    $routine = Routine::findOrFail($routineId);
-    $this->authorize('addExercise', $routine);
-    $exerciseId = $request->input('exercise_id');
-
-    $exerciseRoutine = ExerciseRoutine::query()->where('id', $exerciseId)->where('routine_id', $routineId)->first();
-
-    if (!$exerciseRoutine) {
-        return back()->withErrors(['error' => 'El ejercicio no está en esta rutina']);
-    }
-
-    ExerciseRoutine::query()->where('routine_id', $routineId)->where('id', $exerciseId)->delete();
-
-    switch ($request->input('redirect_to')) {
-        case 'routines.start':
-            return redirect()->route('routines.start', ['id' => $routineId]);
-        case 'routines.edit':
-            return redirect()->route('routines.edit', ['id' => $routineId]);
-        default:
-            return redirect()->route('routines.index');
-    }
-}
-
-public function store()
-{
-    $this->authorize('create', Routine::class);
-    $routine = Routine::create([
-        'user_id' => auth()->id(),
-        'name' => 'Nueva Rutina',
-        'description' => '',
-    ]);
-
-    return redirect()->route('routines.edit', ['id' => $routine->id]);
-}
-
-    public function updateChart(Request $request, $id)
-    {
-        $routine = Routine::findOrFail($id);
-        $this->authorize('updateChart', $routine);
-        $period = $request->input('period', 'month');
-        switch ($period) {
-            case '3months':
-                $startDate = \Illuminate\Support\Carbon::now()->subMonths(3)->startOfDay();
-                break;
-            case 'year':
-                $startDate = \Illuminate\Support\Carbon::now()->subYear()->startOfDay();
-                break;
-            default:
-                $startDate = \Illuminate\Support\Carbon::now()->subMonth()->startOfDay();
-                break;
-        }
-
-        $today = \Illuminate\Support\Carbon::now()->endOfDay();
-
-        $sessions = RoutineSession::where('user_id', auth()->user()->id)
-            ->whereBetween('completed_at', [$startDate, $today])
-            ->with('exerciseLogs')
-            ->get();
-
-        $stadistics = StatisticsResource::collection($sessions)->toArray(request());
-
-        // Guardar en session() para que show lo tome
-        session(['stadistics' => $stadistics]);
-
-        return redirect()->route('routines.show', ['id' => $id]);
-    }
-
-    public function generatePDF($id)
-    {
-        $routine = Routine::findOrFail($id);
-        $this->authorize('generatePDF', $routine);
-        $routineDetails = $this->getRoutineDetails($id);
-        $pdf = Pdf::loadView('pdf.routine', ['routineDetails' => $routineDetails]);
-
-        return $pdf->download("rutina_{$routineDetails['routine']['name']}.pdf");
-    }
-
-    public function destroy($id)
-    {
-        $routine = Routine::findOrFail($id);
-        $this->authorize('delete',$routine);
-        event(new RoutineDeleted($routine));
-        $routine->delete();
-
-        return redirect()->route('routines.index');
-    }
-
-    public function restore($id)
-    {
-        $routine = Routine::onlyTrashed()->findOrFail($id);
-        $this->authorize('delete', $routine);
-        $routine->restore();
-
-        return redirect()->route('routines.index');
     }
 
 }
