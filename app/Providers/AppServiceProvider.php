722c209f6fc47b8a298b146355a9419a40d2e172
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-18 11:31:28 +0100

    Revome Inertia Share from app service provider because it is unused

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,10 +6,8 @@ use App\Http\Middleware\AlwaysAcceptJson;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Routing\Middleware\ThrottleRequests;
 use Illuminate\Routing\Router;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
-use Inertia\Inertia;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 use Spatie\Permission\Middleware\PermissionMiddleware;
 use Spatie\Permission\Middleware\RoleMiddleware;
@@ -39,18 +37,6 @@ class AppServiceProvider extends ServiceProvider
         $router->aliasMiddleware('role', RoleMiddleware::class);
         $router->aliasMiddleware('permission', PermissionMiddleware::class);
         Vite::prefetch(concurrency: 3);
-        Inertia::share([
-            'auth' => function () {
-                return [
-                    'user' => Auth::check() ? [
-                        'id' => Auth::user()->id,
-                        'name' => Auth::user()->name,
-                        'email' => Auth::user()->email,
-                        'role' => Auth::user()->getRoleNames()->first()
-                    ] : null,
-                ];
-            },
-        ]);
 
         $this->app->booted(function () {
             $schedule = app(Schedule::class);
