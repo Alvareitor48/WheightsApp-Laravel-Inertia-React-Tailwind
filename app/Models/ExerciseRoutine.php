3e33198c791a12aad5648d9137be67ce54fb7930
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-22 18:01:51 +0100

    Shoft deletes added to ExerciseRoutine

--- app/Models/ExerciseRoutine.php
+++ app/Models/ExerciseRoutine.php
@@ -6,10 +6,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class ExerciseRoutine extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $table = 'exercises_routines';
+    protected $dates = ['deleted_at'];
     protected $fillable = ['exercise_id','routine_id','note','rest_time'];
 
     public function exercise(): BelongsTo
