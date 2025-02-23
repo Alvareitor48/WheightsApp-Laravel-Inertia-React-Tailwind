fc873a977dab1f710b0cfeae9e8d6fb48504d232
Alvaro Mayol <alvaroelguaperas499@gmail.com>
2025-02-23 22:47:56 +0100

    Error unauth user fixed on exercise policy

--- app/Policies/ExercisePolicy.php
+++ app/Policies/ExercisePolicy.php
@@ -20,8 +20,12 @@ class ExercisePolicy
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Exercise $exercise): bool
+    public function view(?User $user, Exercise $exercise): bool
     {
+        if(!$user){
+            return $exercise->user_id === null;
+        }
+
         if($user->hasRole('user')){
             return $exercise->user_id === null;
         }
